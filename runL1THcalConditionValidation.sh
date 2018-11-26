a6cb752d13f197f514741544c60cac42add5c801
Gilson Correia <gilson.correiasilva@gmail.com>
2018-11-26 14:17:21 +0100

    clean ToRun files

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -3,11 +3,11 @@
 cd ..
 export CMSREL=CMSSW_10_4_0_pre1
 export SCRAM_ARCH=slc6_amd64_gcc630
-scram -a $SCRAM_ARCH project $CMSREL
-cd CMSSW_10_4_0_pre1/src
-cmsenv
-git cms-addpkg CaloOnlineTools/HcalOnlineDb
-scram b -j 16
-cd CaloOnlineTools/HcalOnlineDb/test
-cp /afs/cern.ch/user/c/cawest/public/forGilson/test.py .
-python test.py
+#scram -a $SCRAM_ARCH project $CMSREL
+#cd CMSSW_10_4_0_pre1/src
+#cmsenv
+#git cms-addpkg CaloOnlineTools/HcalOnlineDb
+#scram b -j 16
+#cd CaloOnlineTools/HcalOnlineDb/test
+#cp /afs/cern.ch/user/c/cawest/public/forGilson/test.py .
+#python test.py
