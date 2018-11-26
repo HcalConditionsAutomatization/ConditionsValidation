7da3d463a05bee28b0fee7e98aa1cd8f3d09be83
GilsonCS <gilson.correiasilva@gmail.com>
2018-11-26 14:57:27 -0600

    clean ToRun files

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -14,3 +14,26 @@ scram b -j 16
 cd CaloOnlineTools/HcalOnlineDb/test
 cp ../../../../../HcalConditionsValidation/test.py .
 python test.py
+
+echo "Create the tag"
+
+cd ../../../../..
+export CMSREL=CMSSW_10_2_1
+export SCRAM_ARCH=slc6_amd64_gcc700
+cd CMSSW_10_2_1/src
+eval `scram runtime -sh`
+git cms-init
+git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
+git cms-merge-topic -u cms-l1t-offline:l1t-integration-v101.0
+git cms-addpkg L1Trigger/L1TCommon
+git cms-addpkg L1Trigger/L1TMuon
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
+git cms-addpkg L1Trigger/L1TCalorimeter
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
+scram b -j 16
+scram b -j 16
+cd HcalTrigger/Validation/scripts
+./submit_jobs.py -l lumimask.json -d /ZeroBias/Run2018D-v1/RAW -t HcalL1TriggerObjects_2018_v15.0_data -o T2_BR_SPRACE
+
