00e3abfa025e79c35a7d9c1b067c44c4d3a6671c
GilsonCS <gilson.correiasilva@gmail.com>
2018-11-26 09:03:29 -0600

    clean ToRun files

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -1,13 +1,11 @@
 #!/bin/bash 
 
 cd ..
-pwd
-echo "oi"
-#cmsrel CMSSW_10_4_0_pre1
-#cd CMSSW_10_4_0_pre1/src
-#cmsenv
-#git cms-addpkg CaloOnlineTools/HcalOnlineDb
-#scram b -j 4
-#cd CaloOnlineTools/HcalOnlineDb/test
-#cp /afs/cern.ch/user/c/cawest/public/forGilson/test.py .
-#python test.py
+cmsrel CMSSW_10_4_0_pre1
+cd CMSSW_10_4_0_pre1/src
+cmsenv
+git cms-addpkg CaloOnlineTools/HcalOnlineDb
+scram b -j 16
+cd CaloOnlineTools/HcalOnlineDb/test
+cp /afs/cern.ch/user/c/cawest/public/forGilson/test.py .
+python test.py
