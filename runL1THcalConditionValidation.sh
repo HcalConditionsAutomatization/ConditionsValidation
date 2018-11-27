2c41a72886be6ac92cbeceb94e223ec1f5fb9e5e
GilsonCS <gilson.correiasilva@gmail.com>
2018-11-26 20:00:44 -0600

    clean validation inputs

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -8,7 +8,8 @@ echo "LUTname " + $LUTname
 echo "TAGname " + $TAGname
 echo "release_LUT " + $release_LUT
 echo "arch_LUT " + $arch_LUT
-echo "release_L1 " + $arch_L1
+echo "release_L1 " + $release_L1
+echo "arch_L1 " + $arch_L1
 echo "dataset " + $dataset
 echo "tier2 " + $tier2
 
