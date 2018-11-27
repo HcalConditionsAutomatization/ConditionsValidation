1351f2c3b0f6d0414faa83cc5c7b620877cf2cf3
GilsonCS <gilson.correiasilva@gmail.com>
2018-11-27 12:41:23 -0600

    clean validation inputs

--- new.sh
+++ new.sh
@@ -7,11 +7,13 @@ then
 
 export year=`grep "year" $file | awk '{print $2}'`
 export week=`grep "week" $file | awk '{print $2}'`
-export LUTname=`grep "LUTname" $file | awk '{print $2}'`
-export TAGname=`grep "TAGname" $file | awk '{print $2}'`
-export run=`grep "run" $file | awk '{print $2}'`
+export LUTtag=`grep "LUTtag" $file | awk '{print $2}'`
+export L1TOtag=`grep "L1TOtag" $file | awk '{print $2}'`
+export Run=`grep "Run" $file | awk '{print $2}'`
 export lumi_start=`grep "lumi_start" $file | awk '{print $2}'`
 export lumi_end=`grep "lumi_end" $file | awk '{print $2}'`
+export OldLUTtag=`grep "OldLUTtag" $file | awk '{print $2}'`
+export OldRun=`grep "OldRun" $file | awk '{print $2}'`
 
 export release_LUT=`grep "release_LUT" $file | awk '{print $2}'`
 export arch_LUT=`grep "arch_LUT" $file | awk '{print $2}'`
@@ -25,7 +27,7 @@ export tier2=`grep "tier2" $file | awk '{print $2}'`
 echo {'"'${run}'"': [[$lumi_start, $lumi_end]]} > lumimask.json
 
 ###L1T###
-#./runL1THcalConditionValidation.sh 
+./runL1THcalConditionValidation.sh 
 
 mv $file RunFiles/Validation_${year}_${week}.txt
 cp RunFiles/DefaultValidation.txt NewValidation.txt
--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
 
 echo "==============================================="
-echo "run          " $run
+echo "Run          " $Run
 echo "lumi_section " [$lumi_start,$lumi_end]
-echo "LUTname      " $LUTname
-echo "TAGname      " $TAGname
+echo "LUTtag       " $LUTtag
+echo "L1TOtag      " $L1TOtag
+echo "OldRun       " $OldRun
+echo "OldLUTtag    " $OldLUTtag
 echo "release_LUT  " $release_LUT
 echo "arch_LUT     " $arch_LUT
 echo "release_L1   " $release_L1
@@ -13,36 +15,36 @@ echo "dataset      " $dataset
 echo "tier2        " $tier2
 echo "==============================================="
 
-cd ..
-scram -a $arch_LUT project $release_LUT
-cd CMSSW_10_4_0_pre1/src
-eval `scram runtime -sh`
-git cms-init
-git cms-addpkg CaloOnlineTools/HcalOnlineDb
-scram b -j 16
-scram b -j 16
-cd CaloOnlineTools/HcalOnlineDb/test
-cp ../../../../../HcalConditionsValidation/test.py .
-python test.py $run $LUTname $GT
+#cd ..
+#scram -a $arch_LUT project $release_LUT
+#cd CMSSW_10_4_0_pre1/src
+#eval `scram runtime -sh`
+#git cms-init
+#git cms-addpkg CaloOnlineTools/HcalOnlineDb
+#scram b -j 16
+#scram b -j 16
+#cd CaloOnlineTools/HcalOnlineDb/test
+#cp ../../../../../HcalConditionsValidation/test.py .
+#python test.py $Run $LUTtag $GT
 
-echo "Create the tag"
+#echo "Create the tag"
 
-cd ../../../../..
-scram -a $arch_L1 project $release_L1
-cd CMSSW_10_2_1/src
-eval `scram runtime -sh`
-git cms-init
-git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
-git cms-merge-topic -u cms-l1t-offline:l1t-integration-v101.0
-git cms-addpkg L1Trigger/L1TCommon
-git cms-addpkg L1Trigger/L1TMuon
-git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
-git cms-addpkg L1Trigger/L1TCalorimeter
-git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
-git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
-scram b -j 16
-scram b -j 16
-cd HcalTrigger/Validation/scripts
-./submit_jobs.py -l ../../../../../HcalConditionsValidation/lumimask.json -d $dataset -t $TAGname -o $tier2
+#cd ../../../../..
+#scram -a $arch_L1 project $release_L1
+#cd CMSSW_10_2_1/src
+#eval `scram runtime -sh`
+#git cms-init
+#git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+#git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
+#git cms-merge-topic -u cms-l1t-offline:l1t-integration-v101.0
+#git cms-addpkg L1Trigger/L1TCommon
+#git cms-addpkg L1Trigger/L1TMuon
+#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
+#git cms-addpkg L1Trigger/L1TCalorimeter
+#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+#git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
+#scram b -j 16
+#scram b -j 16
+#cd HcalTrigger/Validation/scripts
+#./submit_jobs.py -l ../../../../../HcalConditionsValidation/lumimask.json -d $dataset -t $L1TOtag -o $tier2
 
