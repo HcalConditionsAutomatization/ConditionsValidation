012df970707fe6de9f7ece346c433d8fdfcc147b
GilsonCS <gilson.correiasilva@gmail.com>
2018-12-12 11:41:50 -0600

    release modification

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -28,9 +28,9 @@ cd ..
 scram -a $arch_LUT project $release_LUT
 cd CMSSW_10_4_0_pre1/src
 eval `scram runtime -sh`
+scram b -j 16
 git cms-init
 git cms-addpkg CaloOnlineTools/HcalOnlineDb
-scram b -j 16
 scram b -j 16
 cd CaloOnlineTools/HcalOnlineDb/test
 cp ../../../../../HcalConditionsValidation/test.py .
@@ -55,19 +55,28 @@ cp HcalL1TriggerObjects.db $outdir/${NewLUTtag}
 #=====================================================================================================================
 cd ../..
 scram -a $arch_L1 project $release_L1
-cd CMSSW_10_2_1/src
+cd CMSSW_10_3_1/src
 eval `scram runtime -sh`
+#git cms-init
+#git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+#git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
+#git cms-merge-topic -u cms-l1t-offline:l1t-integration-v101.0
+#git cms-addpkg L1Trigger/L1TCommon
+#git cms-addpkg L1Trigger/L1TMuon
+#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
+#git cms-addpkg L1Trigger/L1TCalorimeter
+#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
 git cms-init
 git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
-git cms-merge-topic -u cms-l1t-offline:l1t-integration-v101.0
+git fetch cms-l1t-offline l1t-integration-CMSSW_10_3_1
+git cms-merge-topic -u cms-l1t-offline:l1t-integration-v102.1-CMSSW_10_3_1
 git cms-addpkg L1Trigger/L1TCommon
 git cms-addpkg L1Trigger/L1TMuon
 git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
 git cms-addpkg L1Trigger/L1TCalorimeter
 git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
-git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
 scram b -j 16
+git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
 scram b -j 16
 cd HcalTrigger/Validation/scripts
 cp ../../../../../HcalConditionsValidation/submit_jobs.py .
