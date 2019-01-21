54a6142cf9cfd02bd2b3d5db78f69f17c0319d70
GilsonCS <gilson.correiasilva@gmail.com>
2019-01-20 18:55:26 -0600

    Update runL1THcalConditionValidation.sh

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -56,7 +56,6 @@ echo O2OL1TriggerObjects=false >> cardPhysics_gen_old.sh
 echo O2OInputs=false >> cardPhysics_gen_old.sh
 
 cp ../../../../../HcalConditionsValidation/Tools/test.py .
-#cp ../../../../../HcalConditionsValidation/cardPhysics* .
 python test.py $NewRun $NewLUTtag $NewGT $OldRun $OldLUTtag $OldGT   
 cp -r conditions/${NewLUTtag} $outdir
  
@@ -75,10 +74,10 @@ echo " "
 echo "====================================================================================================================="
 echo " L1 rate validation"
 echo "====================================================================================================================="
-#cd ../..
-#scram -a $arch_L1 project $release_L1
-#cd CMSSW_10_2_1/src
-#eval `scram runtime -sh`
+cd ../..
+scram -a $arch_L1 project $release_L1
+cd CMSSW_10_2_1/src
+eval `scram runtime -sh`
 #git cms-init
 #git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
 #git fetch cms-l1t-offline l1t-integration-CMSSW_10_2_1
@@ -89,69 +88,70 @@ echo "==========================================================================
 #git cms-addpkg L1Trigger/L1TCalorimeter
 #git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
 #--------------------------------------------------------------------------------------------------
-#git cms-init
-#git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-#git fetch cms-l1t-offline l1t-integration-CMSSW_10_3_1
-#git cms-merge-topic -u cms-l1t-offline:l1t-integration-v102.1-CMSSW_10_3_1
-#git cms-addpkg L1Trigger/L1TCommon
-#git cms-addpkg L1Trigger/L1TMuon
-#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
-#git cms-addpkg L1Trigger/L1TCalorimeter
-#git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/datascram b -j 16
+git cms-init
+git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+git fetch cms-l1t-offline l1t-integration-CMSSW_10_3_1
+git cms-merge-topic -u cms-l1t-offline:l1t-integration-v102.1-CMSSW_10_3_1
+git cms-addpkg L1Trigger/L1TCommon
+git cms-addpkg L1Trigger/L1TMuon
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
+git cms-addpkg L1Trigger/L1TCalorimeter
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+scram b -j 16
 #----------------------------------------------------------------------------------------------------
-#git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
-#cram b -j 16
-#scram b -j 16
-#cd HcalTrigger/Validation/scripts
-#cp ../../../../../HcalConditionsValidation/Tools/submit_jobs.py .
-##p ../../../../../HcalConditionsValidation/lumimask.json .
-#> lumimask.json
-#echo {'"'${run}'"': [[$lumi_start, $lumi_end]]} > lumimask.json
-#cp ../../../../../CMSSW_10_4_0_pre1/src/HcalL1TriggerObjects.db .
-#./submit_jobs.py -l lumimask.json -d $dataset -t Tag -o $tier2
-#sed -i '/config.JobType.outputFiles/ i\config.JobType.inputFiles = ["HcalL1TriggerObjects.db"]' submit_new_cond.py
+git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
+cram b -j 16
+cd HcalTrigger/Validation/scripts
+cp ../../../../../HcalConditionsValidation/Tools/submit_jobs.py .
+
+> lumimask.json
+echo {'"'${run}'"': [[$lumi_start, $lumi_end]]} > lumimask.json
+
+cp ../../../../../CMSSW_10_4_0_pre1/src/HcalL1TriggerObjects.db .
+./submit_jobs.py -l lumimask.json -d $dataset -t Tag -o $tier2
+sed -i '/config.JobType.outputFiles/ i\config.JobType.inputFiles = ["HcalL1TriggerObjects.db"]' submit_new_cond.py
 
 #------------------------------------------------------------------------------------
 # Submit and retrieve jobs from CRAB
 
-#crab submit submit_def.py
-#crab submit submit_new_cond.py 
+crab submit submit_def.py
+crab submit submit_new_cond.py 
 
-#crab status -d crab_hcal_${run}_def > status_def.log
-#while ! grep -q "finished" status_def.log; do
-#    if grep -q "failed" status_def.log; then
-#        crab resubmit -d crab_hcal_${run}_def
-#    fi
-#    sleep 180
-#    crab status -d crab_hcal_${run}_def > status_def.log
-#done
+crab status -d crab_hcal_${run}_def > status_def.log
+while ! grep -q "finished" status_def.log; do
+    if grep -q "failed" status_def.log; then
+        crab resubmit -d crab_hcal_${run}_def
+    fi
+    sleep 180
+    crab status -d crab_hcal_${run}_def > status_def.log
+done
 
-#crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
-#while ! grep -q "finished" status_new_cond.log; do
-#    if grep -q "failed" status_new_cond.log; then
-#        crab resubmit -d crab_hcal_${run}_new_cond
-#    fi
-#    sleep 180
-#    crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
-#done
+crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
+while ! grep -q "finished" status_new_cond.log; do
+    if grep -q "failed" status_new_cond.log; then
+        crab resubmit -d crab_hcal_${run}_new_cond
+    fi
+    sleep 180
+    crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
+done
 
-#crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
-#while ! grep -q "All files successfully retrieved" retrieve_def.log; do
-#    crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
-#done
+crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
+while ! grep -q "All files successfully retrieved" retrieve_def.log; do
+    crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
+done
 
-#crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
-#while ! grep -q "All files successfully retrieved" retrieve_new_cond.log; do
-#    crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
-#done
+crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
+while ! grep -q "All files successfully retrieved" retrieve_new_cond.log; do
+    crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
+done
 
 #------------------------------------------------------------------------------------
 
-#rates.exe def crab_hcal_${run}_def/results
-#rates.exe new crab_hcal_${run}_new_cond/results
-#mkdir plots
-#draw_rates.exe
-#cp -r plots ${outdir}/${NewLUTtag}
+rates.exe def crab_hcal_${run}_def/results
+rates.exe new crab_hcal_${run}_new_cond/results
+mkdir plots
+draw_rates.exe
+cp -r plots ${outdir}/${NewLUTtag}
 
 
 
