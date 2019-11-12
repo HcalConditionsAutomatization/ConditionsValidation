26f64117af569355d81b78a8dc34c62fb2be8d30
Yuan <54chenyuan@gmail.com>
2019-11-12 11:41:13 +0100

    good, all should work

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -198,9 +198,9 @@ wait
 
 #------------------------------------------------------------------------------------
 
-#rates.exe def ./hcal_${run}_def/
-#rates.exe new ./hcal_${run}_new_cond/
-#mkdir plots
-#draw_rates.exe
-#cp -r plots ${outdir}/${NewLUTtag}
+rates.exe def ./hcal_${run}_def/
+rates.exe new ./hcal_${run}_new_cond/
+mkdir plots
+draw_rates.exe
+cp -r plots ${outdir}/${NewLUTtag}
 
