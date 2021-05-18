dd0d441338c3adaadb6b349779907be7cfc4c6b4
Keane-Tan <chin.lung.t93@gmail.com>
2021-05-18 16:25:20 +0200

    Changed cmsxrootd to cmseos in outdir

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -73,8 +73,7 @@ echo 'eos ls /eos/cms/store/group/dpg_hcal/comm_hcal/chin/'
 eos ls /eos/cms/store/user/rtaus/HCALValidation
 echo conditions/${NewLUTtag}
 #cp -r conditions/${NewLUTtag} $outdir
-xrdcp -rf conditions/${NewLUTtag} root://eoscms.cern.ch//eos/cms/store/user/rtaus/HCALValidation
-eos ls /eos/cms/store/user/rtaus/HCALValidation
+xrdcp -rf conditions/${NewLUTtag} root://cmseos.fnal.gov//store/user/keanet/HCALValidation
 
 # echo " "
 # echo "======================================================================================================================"
