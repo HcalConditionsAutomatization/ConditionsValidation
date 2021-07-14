5b0100232a83b831517f44871ef13288ac73c88c
Keane-Tan <chin.lung.t93@gmail.com>
2021-07-14 20:20:37 +0200

    Uncommented first part of L1TriggerObjects part

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -95,14 +95,14 @@ xrdcp -rf conditions/${NewLUTtag} $outdir
 # echo "======================================================================================================================"
 # echo " L1TriggerObjects Tag generation"
 # echo "======================================================================================================================"
-# cp conditions/$NewLUTtag/Deploy/Gen_L1TriggerObjects_${NewLUTtag}.txt ../../..
-# cd ../../..
-# cp ../../ConditionsValidation/Tools/writetoSQL9x.csh .
-# chmod +x writetoSQL9x.csh
-# ./writetoSQL9x.csh $geometry L1TriggerObjects Gen_L1TriggerObjects_${NewLUTtag}.txt Tag 1 HcalL1TriggerObjects.db
-# xrdcp HcalL1TriggerObjects.db $outdir/${NewLUTtag}/HcalL1TriggerObjects.db
-# echo 'eos ls /eos/cms/store/group/dpg_hcal/comm_hcal/chin/'
-# eos ls $outdir/${NewLUTtag}
+cp conditions/$NewLUTtag/Deploy/Gen_L1TriggerObjects_${NewLUTtag}.txt ../../..
+cd ../../..
+cp ../../ConditionsValidation/Tools/writetoSQL9x.csh .
+chmod +x writetoSQL9x.csh
+./writetoSQL9x.csh $geometry L1TriggerObjects Gen_L1TriggerObjects_${NewLUTtag}.txt Tag 1 HcalL1TriggerObjects.db
+xrdcp HcalL1TriggerObjects.db $outdir/${NewLUTtag}/HcalL1TriggerObjects.db
+echo 'eos ls /eos/cms/store/group/dpg_hcal/comm_hcal/chin/'
+eos ls $outdir/${NewLUTtag}
 
 # echo " "
 # echo "====================================================================================================================="
