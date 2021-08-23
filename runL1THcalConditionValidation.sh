3194a79f1175398e7b1cf6ab82d7a41353c51a30
Keane-Tan <chin.lung.t93@gmail.com>
2021-08-23 17:08:15 +0200

    Testing only the L1TriggerObjects Tag Generation

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -101,124 +101,124 @@ xrdcp -f HcalL1TriggerObjects.db $outdir/${NewLUTtag}/HcalL1TriggerObjects.db
 echo 'eos ls /eos/cms/store/group/dpg_hcal/comm_hcal/chin/'
 eos ls $outdir/${NewLUTtag}
 
-echo " "
-echo "====================================================================================================================="
-echo " L1 rate validation"
-echo "====================================================================================================================="
-cd ../..
-scram -a $arch_L1 project $release_L1
-
-cd ${release_L1}/src
-eval `scram runtime -sh`
-git cms-init
-git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-git fetch cms-l1t-offline l1t-integration-${release_L1}
-git cms-merge-topic -u cms-l1t-offline:l1t-integration-v${version_L1}
-git cms-addpkg L1Trigger/L1TCommon
-git cms-addpkg L1Trigger/L1TMuon
-git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
-git cms-addpkg L1Trigger/L1TCalorimeter
-git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
-scram b -j 8
-#----------------------------------------------------------------------------------------------------
-git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
-scram b -j 8
-cd HcalTrigger/Validation/scripts
-
-mkdir hcal_${run}_def
-mkdir hcal_${run}_new_cond
-cp ../../../../../ConditionsValidation/Tools/ntuple_maker_template.sh ./
-cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db .
-cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db ./hcal_${run}_def
-cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db ./hcal_${run}_new_cond
-if [[ $lumiblock == \#* ]]
-then
-  :
-else
-  echo "{\"$run\": $lumiblock}" > ./lumimask.txt
-  lumimask="../../lumimask.txt"
-fi
-
-dasgoclient -query="file dataset=${dataset} run=${run}" > $listFiles
-n=0
-for file in `less ./${listFiles}`
-do
-  n=$[$n+1]
-  echo "$n. $file"
-  if (( "$n" <= "$max_file_num" )) || (( "$max_file_num" < 0 ))
-  then
-    numfolder=(`find ./hcal_${run}_def/  -maxdepth 1 -name "ntuple_maker_*" -type d | wc -l`)
-    mkdir -p ./hcal_${run}_def/ntuple_maker_$numfolder && mkdir -p ./hcal_${run}_new_cond/ntuple_maker_$numfolder
-    sh ./ntuple_maker_template.sh default $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_def_$n.py ./hcal_${run}_def/ntuple_maker_$numfolder
-    sh ./ntuple_maker_template.sh new_con $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_new_$n.py ./hcal_${run}_new_cond/ntuple_maker_$numfolder
-    ( cd ./hcal_${run}_def/ntuple_maker_$numfolder && cmsRun ntuple_maker_def_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) & ( cd ./hcal_${run}_new_cond/ntuple_maker_$numfolder && cmsRun ntuple_maker_new_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) &
-#    wait
-    if [ $(jobs | wc -l) -ge $jobs_in_parallel ]; then
-      echo "Waiting for background processes to finish ..."
-      wait
-      rm -r ./hcal_${run}_def/ntuple_maker_*
-      rm -r ./hcal_${run}_new_cond/ntuple_maker_*
-    fi
-  else
-    break
-  fi
-done
-echo "Waiting for background processes to finish ..."
-wait
-
-cp ../../../../../ConditionsValidation/Tools/runcrab3.csh .
-source runcrab3.csh
-
-#------------------------------------------------------------------------------------
-#Submit and retrieve jobs from CRAB
-cp ../../../../../ConditionsValidation/Tools/ntuple_maker_def.py .
-cmsRun ntuple_maker_def.py
-cp submit_def.py $outdir
-crab submit submit_def.py
-crab preparelocal --dir='crab_hcal_325170_def'
-cp -r crab_hcal_325170_def $outdir
-
-cp ntuple_maker_def.py $outdir
-source /cvmfs/cms.cern.ch/crab3/crab.sh
-
-
-crab submit submit_def.py
-crab submit submit_new_cond.py
-cp crab_hcal_325170_new_cond/crab.log ${outdir}/${NewLUTtag}
-cp crab_hcal_325170_def/crab.log ${outdir}/${NewLUTtag}
-crab status -d crab_hcal_${run}_def
-crab status -d crab_hcal_${run}_def > status_def.log
-while ! grep -q "finished" status_def.log; do
-   if grep -q "failed" status_def.log; then
-       crab resubmit -d crab_hcal_${run}_def
-   fi
-   sleep 180
-   crab status -d crab_hcal_${run}_def > status_def.log
-done
-
-crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
-while ! grep -q "finished" status_new_cond.log; do
-   if grep -q "failed" status_new_cond.log; then
-       crab resubmit -d crab_hcal_${run}_new_cond
-   fi
-   sleep 180
-   crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
-done
-
-crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
-while ! grep -q "All files successfully retrieved" retrieve_def.log; do
-   crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
-done
-
-crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
-while ! grep -q "All files successfully retrieved" retrieve_new_cond.log; do
-   crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
-done
-
-# ------------------------------------------------------------------------------------
-
-rates.exe def ./hcal_${run}_def/
-rates.exe new ./hcal_${run}_new_cond/
-mkdir plots
-draw_rates.exe
-xrdcp -r plots ${outdir}/${NewLUTtag}
+# echo " "
+# echo "====================================================================================================================="
+# echo " L1 rate validation"
+# echo "====================================================================================================================="
+# cd ../..
+# scram -a $arch_L1 project $release_L1
+#
+# cd ${release_L1}/src
+# eval `scram runtime -sh`
+# git cms-init
+# git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+# git fetch cms-l1t-offline l1t-integration-${release_L1}
+# git cms-merge-topic -u cms-l1t-offline:l1t-integration-v${version_L1}
+# git cms-addpkg L1Trigger/L1TCommon
+# git cms-addpkg L1Trigger/L1TMuon
+# git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
+# git cms-addpkg L1Trigger/L1TCalorimeter
+# git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+# scram b -j 8
+# #----------------------------------------------------------------------------------------------------
+# git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
+# scram b -j 8
+# cd HcalTrigger/Validation/scripts
+#
+# mkdir hcal_${run}_def
+# mkdir hcal_${run}_new_cond
+# cp ../../../../../ConditionsValidation/Tools/ntuple_maker_template.sh ./
+# cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db .
+# cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db ./hcal_${run}_def
+# cp ../../../../../$release_LUT/src/HcalL1TriggerObjects.db ./hcal_${run}_new_cond
+# if [[ $lumiblock == \#* ]]
+# then
+#   :
+# else
+#   echo "{\"$run\": $lumiblock}" > ./lumimask.txt
+#   lumimask="../../lumimask.txt"
+# fi
+#
+# dasgoclient -query="file dataset=${dataset} run=${run}" > $listFiles
+# n=0
+# for file in `less ./${listFiles}`
+# do
+#   n=$[$n+1]
+#   echo "$n. $file"
+#   if (( "$n" <= "$max_file_num" )) || (( "$max_file_num" < 0 ))
+#   then
+#     numfolder=(`find ./hcal_${run}_def/  -maxdepth 1 -name "ntuple_maker_*" -type d | wc -l`)
+#     mkdir -p ./hcal_${run}_def/ntuple_maker_$numfolder && mkdir -p ./hcal_${run}_new_cond/ntuple_maker_$numfolder
+#     sh ./ntuple_maker_template.sh default $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_def_$n.py ./hcal_${run}_def/ntuple_maker_$numfolder
+#     sh ./ntuple_maker_template.sh new_con $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_new_$n.py ./hcal_${run}_new_cond/ntuple_maker_$numfolder
+#     ( cd ./hcal_${run}_def/ntuple_maker_$numfolder && cmsRun ntuple_maker_def_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) & ( cd ./hcal_${run}_new_cond/ntuple_maker_$numfolder && cmsRun ntuple_maker_new_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) &
+# #    wait
+#     if [ $(jobs | wc -l) -ge $jobs_in_parallel ]; then
+#       echo "Waiting for background processes to finish ..."
+#       wait
+#       rm -r ./hcal_${run}_def/ntuple_maker_*
+#       rm -r ./hcal_${run}_new_cond/ntuple_maker_*
+#     fi
+#   else
+#     break
+#   fi
+# done
+# echo "Waiting for background processes to finish ..."
+# wait
+#
+# cp ../../../../../ConditionsValidation/Tools/runcrab3.csh .
+# source runcrab3.csh
+#
+# #------------------------------------------------------------------------------------
+# #Submit and retrieve jobs from CRAB
+# cp ../../../../../ConditionsValidation/Tools/ntuple_maker_def.py .
+# cmsRun ntuple_maker_def.py
+# cp submit_def.py $outdir
+# crab submit submit_def.py
+# crab preparelocal --dir='crab_hcal_325170_def'
+# cp -r crab_hcal_325170_def $outdir
+#
+# cp ntuple_maker_def.py $outdir
+# source /cvmfs/cms.cern.ch/crab3/crab.sh
+#
+#
+# crab submit submit_def.py
+# crab submit submit_new_cond.py
+# cp crab_hcal_325170_new_cond/crab.log ${outdir}/${NewLUTtag}
+# cp crab_hcal_325170_def/crab.log ${outdir}/${NewLUTtag}
+# crab status -d crab_hcal_${run}_def
+# crab status -d crab_hcal_${run}_def > status_def.log
+# while ! grep -q "finished" status_def.log; do
+#    if grep -q "failed" status_def.log; then
+#        crab resubmit -d crab_hcal_${run}_def
+#    fi
+#    sleep 180
+#    crab status -d crab_hcal_${run}_def > status_def.log
+# done
+#
+# crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
+# while ! grep -q "finished" status_new_cond.log; do
+#    if grep -q "failed" status_new_cond.log; then
+#        crab resubmit -d crab_hcal_${run}_new_cond
+#    fi
+#    sleep 180
+#    crab status -d crab_hcal_${run}_new_cond > status_new_cond.log
+# done
+#
+# crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
+# while ! grep -q "All files successfully retrieved" retrieve_def.log; do
+#    crab getoutput -d crab_hcal_${run}_def --checksum=no > retrieve_def.log
+# done
+#
+# crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
+# while ! grep -q "All files successfully retrieved" retrieve_new_cond.log; do
+#    crab getoutput -d crab_hcal_${run}_new_cond --checksum=no > retrieve_new_cond.log
+# done
+#
+# # ------------------------------------------------------------------------------------
+#
+# rates.exe def ./hcal_${run}_def/
+# rates.exe new ./hcal_${run}_new_cond/
+# mkdir plots
+# draw_rates.exe
+# xrdcp -r plots ${outdir}/${NewLUTtag}
