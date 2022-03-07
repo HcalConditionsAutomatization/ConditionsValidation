5325ebff14a9eb6151042690b7cad96c286d6c0a
Keane-Tan <chin.lung.t93@gmail.com>
2022-03-07 01:49:08 +0100

    Fixed hardcoded problems for L1 setup, changed setup to match Taeun

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -112,13 +112,10 @@ cd ${release_L1}/src
 eval `scram runtime -sh`
 git cms-init
 git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-git fetch cms-l1t-offline l1t-integration-CMSSW_11_2_0
+git fetch cms-l1t-offline l1t-integration-${release_L1}
 git cms-merge-topic -u cms-l1t-offline:l1t-integration-v${version_L1}
-git cms-addpkg L1Trigger/L1TCommon
-git cms-addpkg L1Trigger/L1TMuon
-git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
-git cms-addpkg L1Trigger/L1TCalorimeter
 git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+git cms-checkdeps -A -a
 scram b -j 8
 #----------------------------------------------------------------------------------------------------
 git clone git@github.com:cms-hcal-trigger/Validation.git HcalTrigger/Validation
@@ -151,8 +148,8 @@ do
   then
     numfolder=(`find ./hcal_${run}_def/  -maxdepth 1 -name "ntuple_maker_*" -type d | wc -l`)
     mkdir -p ./hcal_${run}_def/ntuple_maker_$numfolder && mkdir -p ./hcal_${run}_new_cond/ntuple_maker_$numfolder
-    sh ./ntuple_maker_template.sh default $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_def_$n.py ./hcal_${run}_def/ntuple_maker_$numfolder
-    sh ./ntuple_maker_template.sh new_con $n $nEvts Run2_2018 101X_dataRun2_HLT_v7 root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_new_$n.py ./hcal_${run}_new_cond/ntuple_maker_$numfolder
+    sh ./ntuple_maker_template.sh default $n $nEvts Run3 $NewGT root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_def_$n.py ./hcal_${run}_def/ntuple_maker_$numfolder
+    sh ./ntuple_maker_template.sh new_con $n $nEvts Run3 $NewGT root://cms-xrd-global.cern.ch//$file $lumimask && mv ntuple_maker_new_$n.py ./hcal_${run}_new_cond/ntuple_maker_$numfolder
     ( cd ./hcal_${run}_def/ntuple_maker_$numfolder && cmsRun ntuple_maker_def_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) & ( cd ./hcal_${run}_new_cond/ntuple_maker_$numfolder && cmsRun ntuple_maker_new_$n.py && mv L1Ntuple.root ../L1Ntuple_$n.root ) &
 #    wait
     if [ $(jobs | wc -l) -ge $jobs_in_parallel ]; then
