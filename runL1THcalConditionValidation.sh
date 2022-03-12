47aee87dd92aedcbba47726ebb5e3ec26b5db209
Keane-Tan <chin.lung.t93@gmail.com>
2022-03-12 18:07:04 +0100

    Fixed the destination for L1 plots

--- runL1THcalConditionValidation.sh
+++ runL1THcalConditionValidation.sh
@@ -174,4 +174,5 @@ rates.exe new ./hcal_${run}_new_cond/
 mkdir plots
 draw_rates.exe
 ls plots
-xrdcp -rf plots ${outdir}/${NewLUTtag}/plots
+eos mkdir ${outdir}/${NewLUTtag}/L1Plots
+xrdcp -rf plots ${outdir}/${NewLUTtag}/L1Plots
