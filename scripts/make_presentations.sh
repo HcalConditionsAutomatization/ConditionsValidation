960e929340cfdbe5e6170b11d8f52c828be56738
Michael-Krohn <krohn045@umn.edu>
2022-11-29 13:44:38 -0600

    debugging

--- scripts/make_presentations.sh
+++ scripts/make_presentations.sh
@@ -7,4 +7,8 @@ cd $BASE_PATH/ConditionsValidation
 
 python3 -m venv env
 source env/bin/activate
+cd ${release_L1}/src
+cmsenv
+cd $BASE_PATH/ConditionsValidation
+
 python3 -m PostAnalysis configuration/post_conf.py
