74acae716786771247acdcf9d147860b879f9cf3
Michael David Krohn <michael.krohn@colorado.edu>
2023-05-02 00:41:52 +0200

    additional info to slides

--- PostAnalysis/make_overview.py
+++ PostAnalysis/make_overview.py
@@ -15,6 +15,12 @@ def makeOverviewSlides(config):
     old_rates=(config.analyzer_output/"rates"/"rates_def.root").resolve()
     new_rates=(config.analyzer_output/"rates"/"rates_new_cond.root").resolve()
     git_commit = config.commit.strip()
+    cmsswl1 = config.CMSSWl1.strip()
+    dataset = config.DATASETraw.strip()
+    l1run = config.L1RUN.strip()
+    lumistart = config.LUMISTART.strip()
+    lumiend = config.LUMIEND.strip()
+    gt = config.GT.strip()
 
     rate_changes= [[ trigger, round(getRatio(new_rates,old_rates,r[0], r[1], r[2]),3) ] 
             for trigger, r in changes.items()]
@@ -32,7 +38,13 @@ def makeOverviewSlides(config):
         "images" :  plots,
         "max_change" : max_change,
         "bad_changes": anomalous_changes,
-        "commit" : git_commit
+        "commit" : git_commit,
+        "cmsswl1" : cmsswl1,
+        "dataset" : dataset,
+        "l1run" : l1run,
+        "lumistart" : lumistart,
+        "lumiend" : lumiend,
+        "gt" : gt
         })
     tm.compile()
 
